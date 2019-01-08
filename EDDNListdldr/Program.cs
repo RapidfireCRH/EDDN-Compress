66fc14dd98c717ac4052a97268d9cc0e6846c416
RapidfireCRH <Rapidfire882007@live.com>
2019-01-07 22:48:41 -0700

    Cleaned up code a bit added date to /m param so app can name file correctly cleaned up error code so exceptions are not generated, just declares what is wrong and closes gracefully added lines for each step to communicate where in the process the app is at. added custom 7zip location added version numbers added about/version

--- EDDNListdldr/Program.cs
+++ EDDNListdldr/Program.cs
@@ -9,33 +9,62 @@ namespace EDDNListdldr
     {
         static string workingdir = Path.Combine(Directory.GetCurrentDirectory(), "working");
         static string pythonpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Programs\Python\Python37\python.exe");
+        public static bool manual = false;
+        public static DateTime manual_date = DateTime.UtcNow;
+        public static string ziploc = "\"C:\\Program Files\\7-Zip\\7z.exe\"";
+        static WebClient Client = new WebClient();
+        //get date filename
+        static string filenamedate = DateTime.UtcNow.AddDays(-1).ToString("yyyy-MM-dd") + ".jsonl"; //2019-01-04.jsonl
+        static string filename7z = filenamedate + ".7z";//2019-01-04.jsonl.7z
+        static int version_major = 1;
+        static int version_minor = 0;
+
         static void Main(string[] args)
         {
-            //get date filename
-            string filenamedate = DateTime.UtcNow.AddDays(-1).ToString("yyyy-MM-dd") + ".jsonl"; //2019-01-04.jsonl
-            string filename7z = filenamedate + ".7z";//2019-01-04.jsonl.7z
-
+            if(args.Length == 1 && args[0].Length > 2 && (args[0].ToLower().Substring(0,2) == "/v" || args[0].ToLower().Substring(0, 2) == "/a"))
+            {
+                Console.WriteLine("EDDN Compress Version " + version_major + "." + version_minor);
+                Console.WriteLine("Written by RapidfireCRH. https://github.com/RapidfireCRH/EDDN-Compress");
+            }
             //if there is a /M(anuel) flag, move all args over. Otherwise assume SOP.
+            Console.WriteLine("EDDN Compress Version "+version_major+"."+version_minor+" - Starting process.");
             if (!(args[0] == "/m"))
             {
+                Console.Write("Checking for required files. ");
                 //clean up working directory and check that required files 
-                WebClient Client = new WebClient();
+                // Throw an error if listener does not exist. FATAL ERROR
+                if (!File.Exists("listeners.txt"))
+                {
+                    File.AppendAllText("error.log", Environment.NewLine + DateTime.Now.ToString() + " - Error: No Listener file. Please populate a list of listeners that will be used to make a master file and save to listener.txt in the same directory as this execuitable.");
+                    Console.WriteLine("File listener.txt does not exist. Please populate a list of listeners that will be used to make a master file and save to listener.txt in the same directory as this execuitable");
+                    return;
+                }
+
+                // Throw an Error if 7zip is not installed and standalone is not in the same directory FATAL ERROR
+                if (!(File.Exists(@"C:\Program Files\7-Zip\7z.exe") && !(File.Exists(Path.Combine(Directory.GetCurrentDirectory(),"7za.exe")))))
+                {
+                    File.AppendAllText("error.log", Environment.NewLine + DateTime.Now.ToString() + " - Error: 7zip 64 - bit not installed. Please install the latest version of 7zip and run this app again.");
+                    Console.WriteLine("Error: 7zip 64 - bit not installed.Please install the latest version of 7zip and run this app again.");
+                    return;
+                }
+                else if (File.Exists("7za.exe"))//file is standalone in the same directory
+                    ziploc = Path.Combine(Directory.GetCurrentDirectory(), "7za.exe");
+                
                 if (!File.Exists("eddn-merger.py"))// download parser if necessary
                     Client.DownloadFile("https://gist.githubusercontent.com/Thurion/28c109fbe49cf4e9add2811a73303903/raw/86717aa7c3054cb11a32a4f09f76cf441ed9dd26/eddn-merger.py", "eddn-merger.py");
-                if (!File.Exists("listeners.txt"))// Throw an error if listener does not exist
-                    throw new IOException("File listener.txt does not exist. Please populate a list of listeners that will be used to make a master file and save to listener.txt in the same directory as this execuitable");
-                if (!File.Exists(@"C:\Program Files\7-Zip\7z.exe"))
-                    throw new IOException("7zip 64-bit not installed. Exiting...");
+
                 if (!Directory.Exists(workingdir))//create working directory
                     Directory.CreateDirectory(workingdir);
                 if (!Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "complete")))
                     Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "complete"));
-
+                Console.WriteLine("Completed.");
+                Console.Write("Cleaning any residual files. ");
                 //cleanup folders of all files
                 delete(filename7z);
                 delete("*.jsonl");
                 delete("listener.log");
                 delete("*.jsonl", workingdir);
+                Console.WriteLine("Completed.");
 
                 //compile list of listeners. ignore ones with a # as the first charactor
                 List<string> listeners = new List<string>();
@@ -43,6 +72,7 @@ namespace EDDNListdldr
                     if (x.Length != 0 && x[0] != '#')
                         listeners.Add(x);
 
+                Console.Write("Downloading files from " + listeners.Count + " locations. ");
                 //download each file
                 foreach (string x in listeners)
                 {
@@ -55,21 +85,37 @@ namespace EDDNListdldr
                     }
                     catch (Exception e) { File.AppendAllText("error.log", Environment.NewLine + DateTime.Now.ToString() + " - Error: " + e.Message + ". address = " + x + "/" + filename7z); }
                 }
+
+                //Check that there are files to process
                 var dir = new DirectoryInfo(workingdir);
                 int i = 0;
                 foreach (var file in dir.EnumerateFiles())
                     i++;
-                if (i == 0)
-                    throw new Exception("Unable to proceed, no files to process");
+                if (i == 0)// fail if there are no files. FATAL ERROR
+                {
+                    File.AppendAllText("error.log", Environment.NewLine + DateTime.Now.ToString() + " - Error: No files to process. Are the listeners done compressing the files yet?");
+                    Console.WriteLine("Error: No files to process. Are the listeners done compressing the files yet? ");
+                    return;
+                }
+                Console.WriteLine("Completed.");
             }
             else
             {
-                string[] temp = new string[args.Length - 1];
-                for (int j = 1; j != args.Length; j++)
-                    temp[j-1] = args[j];
+                Console.WriteLine("Manual Switch detected. Running current files in work directory.");
+                //if the first arg is /m, expect date in second arg
+                manual = true;
+                try{ manual_date = DateTime.Parse(args[1]); }
+                catch { File.AppendAllText("error.log", Environment.NewLine + DateTime.Now.ToString() + " - Error: Unable to parce " + args[1] + " into a date."); }
+                string[] temp = new string[args.Length - 2];
+                for (int j = 2; j != args.Length; j++)
+                    temp[j-2] = args[j];
                 args = temp;
+
+                filenamedate = manual_date.ToString("yyyy-MM-dd") + ".jsonl";
+                filename7z = filenamedate + ".7z";
             }
 
+            Console.Write("Running compare script (This will take a while). ");
             //run compare and compress script.
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
@@ -79,55 +125,83 @@ namespace EDDNListdldr
             process.StartInfo = startInfo;
             process.Start();
             process.WaitForExit();
-            if (process.ExitCode != 0)
-                File.AppendAllText("error.log", Environment.NewLine + DateTime.Now.ToString() + " - Error: eddn-merger.py failed (Exit Code "+process.ExitCode+")");
-
-            //if file does not exist, give helpful message about possible causes
-            if (!File.Exists("messages.jsonl"))
-                throw new IOException("Script did not complete successfully. Make sure that you have Python 3.7 installed and have installed tqdm, python-dateutil from pip.");
-
+            if (process.ExitCode != 0 || !File.Exists("messages.jsonl"))//Check for fail on app. FATAL ERROR
+            {
+                File.AppendAllText("error.log", Environment.NewLine + DateTime.Now.ToString() + " - Error: eddn-merger.py failed (Exit Code " + process.ExitCode + ")");
+                if (process.ExitCode != 0)
+                    Console.WriteLine(" Failed: script returned with the following error code: " + process.ExitCode + ". Make sure that you have Python 3.7 installed and have installed tqdm, python-dateutil from pip.");
+                else
+                    Console.WriteLine(" Failed: script completed but expected file was not found");
+                return;
+            }
+            Console.WriteLine("Completed.");
+            Console.Write("Cleaning files. ");
             //clean working directory and prep jsonl for compress
             delete("*.jsonl", workingdir);
             File.Move("messages.jsonl", filenamedate);
 
-            //compress file
-            for (int j = 0; j != args.Length; j++)
-                if (args[j].Contains("%MMM%"))
-                    args[j] = args[j].Replace("%MMM%", (DateTime.UtcNow.AddDays(-1).ToString("MMM") + ".7z"));
-            foreach (string x in args)
+            Console.WriteLine("Completed.");
+            if (args.Length != 0)//If a file location is specified.
             {
+                Console.Write("Compressing files to specified folders. ");
+                //compress file
+                for (int j = 0; j != args.Length; j++)
+                    if (args[j].Contains("%MMM%"))
+                        args[j] = args[j].Replace("%MMM%", (DateTime.UtcNow.AddDays(-1).ToString("MMM") + ".7z"));
+                foreach (string x in args)
+                {
+                    try
+                    {
+                        System.Diagnostics.Process process2 = new System.Diagnostics.Process();
+                        System.Diagnostics.ProcessStartInfo startInfo2 = new System.Diagnostics.ProcessStartInfo();
+                        startInfo2.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                        startInfo2.FileName = ziploc;
+                        try
+                        {
+                            FileAttributes attr = File.GetAttributes(x);
+                            if (attr.HasFlag(FileAttributes.Directory))         //is a directory and need a file name
+                                startInfo2.Arguments = "a -y \"" + Path.Combine(x, filename7z) + "\" " + filenamedate;
+                            else if (x.Substring(x.Length - 3, 3) == ".7z")     //has a file name already
+                                startInfo2.Arguments = "a \"" + x + "\" " + filenamedate;
+                            else                                                // file doesnt exist and none was inlcuded. needs a file name
+                                startInfo2.Arguments = "a \"" + Path.Combine(x, filename7z) + "\" " + filenamedate;
+                        }
+                        catch
+                        {
+                            if (x.Substring(x.Length - 3, 3) == ".7z")          //has a file name already
+                                startInfo2.Arguments = "a \"" + x + "\" " + filenamedate;
+                            else                                                //file doesnt exist and none was inlcuded. needs a file name
+                                startInfo2.Arguments = "a \"" + Path.Combine(x, filename7z) + "\" " + filenamedate;
+                        }
+                        process2.StartInfo = startInfo2;
+                        process2.Start();
+                        process2.WaitForExit();
+                        if (process2.ExitCode != 0)
+                            File.AppendAllText("error.log", Environment.NewLine + DateTime.Now.ToString() + " - Error: 7zip unable to zip file. (Exit Code " + process2.ExitCode + ")");
+                    }
+                    catch (Exception e) { File.AppendAllText("error.log", Environment.NewLine + DateTime.Now.ToString() + " - Error: Unable to write file. " + e.Message); }
+                }
+            }
+            else//If no file specified, put completed product in the completed folder.
+            {
+                Console.Write("Compressing files to completed folder. ");
                 try
                 {
                     System.Diagnostics.Process process2 = new System.Diagnostics.Process();
                     System.Diagnostics.ProcessStartInfo startInfo2 = new System.Diagnostics.ProcessStartInfo();
                     startInfo2.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
                     startInfo2.FileName = "\"C:\\Program Files\\7-Zip\\7z.exe\"";
-                    try
-                    {
-                        FileAttributes attr = File.GetAttributes(x);
-                        if (attr.HasFlag(FileAttributes.Directory))         //is a directory and need a file name
-                            startInfo2.Arguments = "a -y \"" + Path.Combine(x, filename7z) + "\" " + filenamedate;
-                        else if (x.Substring(x.Length - 3, 3) == ".7z")           //has a file name already
-                            startInfo2.Arguments = "a \"" + x + "\" " + filenamedate;
-                        else
-                            startInfo2.Arguments = "a \"" + Path.Combine(x, filename7z) + "\" " + filenamedate;
-                    }
-                    catch
-                    {
-                        if (x.Substring(x.Length - 3, 3) == ".7z")           //has a file name already
-                            startInfo2.Arguments = "a \"" + x + "\" " + filenamedate;
-                        else                                                //file doesnt exist and none was inlcuded. needs a file name
-                            startInfo2.Arguments = "a \"" + Path.Combine(x, filename7z) + "\" " + filenamedate;
-                    }
+                    startInfo2.Arguments = "a \"" + Path.Combine(Directory.GetCurrentDirectory(), "completed", filename7z) + "\" " + filenamedate;
                     process2.StartInfo = startInfo2;
                     process2.Start();
                     process2.WaitForExit();
                     if (process2.ExitCode != 0)
                         File.AppendAllText("error.log", Environment.NewLine + DateTime.Now.ToString() + " - Error: 7zip unable to zip file. (Exit Code " + process2.ExitCode + ")");
                 }
-                catch(Exception e) { File.AppendAllText("error.log", Environment.NewLine + DateTime.Now.ToString() + " - Error: Unable to write file. " + e.Message); }
+                catch (Exception e) { File.AppendAllText("error.log", Environment.NewLine + DateTime.Now.ToString() + " - Error: Unable to write file. " + e.Message); }
             }
             //Clean and move completed file
+            Console.WriteLine("Completed.");
             delete("*.jsonl");
         }
 
@@ -150,7 +224,7 @@ namespace EDDNListdldr
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
             startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            startInfo.FileName = @"C:\Program Files\7-Zip\7z.exe";
+            startInfo.FileName = ziploc;
             startInfo.Arguments = "e -o\"" + Path.Combine(workingdir,enddir) + "\" -aou \"" + Path.Combine(path, filename) + "\"";//-o for output directory, -aou to rename writting file if file exists
             process.StartInfo = startInfo;
             process.Start();
